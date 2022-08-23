5becdbacd3572651f424436cdb7adfa4cd861db5
sixerdoodle <tom.struzik@gmail.com>
2022-08-23 12:44:45 -0500

    Update MMM-GoogleCalendar.js
    duplicate calendar events are not trapped as event.start is an object thus parseInt fails.  using event.startDate appears to work properly

--- MMM-GoogleCalendar.js
+++ MMM-GoogleCalendar.js
@@ -737,7 +737,7 @@ Module.register("MMM-GoogleCalendar", {
     for (const evt of eventList) {
       if (
         evt.summary === event.summary &&
-        parseInt(evt.start) === parseInt(event.start)
+        parseInt(evt.startDate) === parseInt(event.startDate)  // event.start is an object ParseInt fails, startDate works
       ) {
         return true;
       }
